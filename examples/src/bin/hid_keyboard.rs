0eecf87dde834d1a32c7fae748fa07c74fdd9e1a
Adam Simpkins <adam@adamsimpkins.net>
2024-04-02 13:55:57 -0700

    [example] include timestamps in the log messages

--- examples/src/bin/hid_keyboard.rs
+++ examples/src/bin/hid_keyboard.rs
@@ -18,7 +18,11 @@ use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 
 #[main]
 async fn main(_spawner: Spawner) {
-    esp_println::logger::init_logger(log::LevelFilter::Trace);
+    // esp_println::logger::init_logger(log::LevelFilter::Trace);
+    unsafe {
+        log::set_logger_racy(&TimestampLogger).unwrap();
+        log::set_max_level_racy(log::LevelFilter::Trace);
+    }
 
     log::info!("Starting HID keyboard example...");
 
@@ -44,7 +48,7 @@ async fn main(_spawner: Spawner) {
     log::info!("driver created");
 
     // Create embassy-usb Config
-    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    let mut config = embassy_usb::Config::new(0x6666, 0x6666);
     config.manufacturer = Some("Embassy");
     config.product = Some("HID keyboard example");
     config.serial_number = Some("12345678");
@@ -201,4 +205,38 @@ impl Handler for MyDeviceHandler {
             log::info!("Device is no longer configured, the Vbus current limit is 100mA.");
         }
     }
+}
+
+struct TimestampLogger;
+
+impl log::Log for TimestampLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        use esp_println::println;
+
+        const RESET: &str = "\u{001B}[0m";
+        const RED: &str = "\u{001B}[31m";
+        const GREEN: &str = "\u{001B}[32m";
+        const YELLOW: &str = "\u{001B}[33m";
+        const BLUE: &str = "\u{001B}[34m";
+        const CYAN: &str = "\u{001B}[35m";
+
+        let color = match record.level() {
+            log::Level::Error => RED,
+            log::Level::Warn => YELLOW,
+            log::Level::Info => GREEN,
+            log::Level::Debug => BLUE,
+            log::Level::Trace => CYAN,
+        };
+
+        let timestamp = embassy_time::Instant::now();
+        let s = timestamp.as_secs();
+        let ms = timestamp.as_millis() % 1000;
+        println!("{}{}.{:03}: {} - {}{}", color, s, ms, record.level(), record.args(), RESET);
+    }
+
+    fn flush(&self) {}
 }
